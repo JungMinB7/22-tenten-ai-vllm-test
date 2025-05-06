e1c33b921a477ddea8165e8800989bbc3006810d
Eul Ka <74575977+kaeul1020@users.noreply.github.com>
2025-05-06 22:54:11 +0900

    [DOCS] api 명세서에 부합하도록 schema 추가 생성 및 수정

--- schemas/bot_recomments_schema.py
+++ schemas/bot_recomments_schema.py
@@ -1,17 +1,24 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 from schemas.bot_common_schema import UserInfoResponse, BaseMessageRequest
- 
+
+class PostRequest(BaseMessageRequest):
+    id: int
+
+class CommentWithRecommentsRequest(BaseMessageRequest):
+    id: int
+    # recomments 배열이 비어있을 수 있으므로 기본값을 빈 리스트로 설정
+    recomments: Optional[List[BaseMessageRequest]] = []
 
 class BotRecommentsRequest(BaseModel):
     board_type: str
-    post: BaseMessageRequest
-    parent_comment_id: int
-    comments: List[BaseMessageRequest]
+    post: PostRequest
+    comment: CommentWithRecommentsRequest
 
 class BotRecommentResponseData(BaseModel):
     board_type: str
-    parent_comment_id: int
+    post_id: int
+    comment_id: int
     user: UserInfoResponse
     content: str
 
